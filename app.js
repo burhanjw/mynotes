8002c3936af761c49c4c9bc9555c584ae82b2cbb
burhanjw <burhanjw@gmail.com>
2017-03-04 21:29:43 +0530

    Update for Enabling Edition of Notes
    Added Method and Variables for Notes Edition

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var app = new Vue({
-   el : '#container',	
-	    data: {
-		     newNote	: '',
-		     Notes 	  : ['This is my First Note, Need to Complete Teemac assignment before 7th March... ']
-	    },
-	    methods :{
-		     addNote() {
-			      this.Notes.push(this.newNote);
-			      this.newNote = '';
-		     }	
-	   },
+	el : '#container',	
+	data: {
+		operation : 'add',
+		Note	: '',
+		Notes 	: ['This is my First Note, Need to Complete Teemac assignment before 7th March... '],
+		CurrEdit: 0,
+	},
+	methods :{
+		addNote() {
+			if(this.Note != ''){			
+				this.Notes.push(this.Note);
+				this.Note = '';
+			}
+		},
+		editNote(Note) {
+			this.Note = Note;		
+			this.operation = 'edit';
+			this.CurrEdit = this.Notes.indexOf(Note);
+		},
+		updateNote() {
+			if(this.Note != ''){			
+				Vue.set(this.Notes, this.CurrEdit , this.Note);			
+				this.Note = '';
+				this.operation = 'add';
+			}
+		}
+			
+	},
+	
 })
